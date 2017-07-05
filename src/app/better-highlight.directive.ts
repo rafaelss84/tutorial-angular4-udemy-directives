9ace2953bc51e026d775f95a878cc82816040c4e
RAFAEL SOUZA DA SILVA <rafaelss@ciasc.sc.gov.br>
2017-07-05 15:38:23 -0300

    Aula 89: Binding to Directive Properties

--- src/app/better-highlight.directive.ts
+++ src/app/better-highlight.directive.ts
@@ -1,16 +1,23 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
+  @Input()
+  defaultColor: string;
+
+  @Input('appBetterHighlight')
+  highlightColor: string = 'blue';
+
   @HostBinding('style.backgroundColor') 
-  backgroundColor: string = 'transparent';
+  backgroundColor: string = this.defaultColor;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
     // this.renderer.setStyle(
     //   this.elementRef.nativeElement, 
     //   'background-color', 'blue')
@@ -22,7 +29,7 @@ export class BetterHighlightDirective implements OnInit {
     //   this.elementRef.nativeElement, 
     //   'background-color', 
     //   'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') 
@@ -31,7 +38,7 @@ export class BetterHighlightDirective implements OnInit {
     //   this.elementRef.nativeElement, 
     //   'background-color', 
     //   'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 }
